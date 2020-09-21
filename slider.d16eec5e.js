3578a79a4d128130d8fcb55329aded4230f28234
Iryna Zalata <iryna.zalata@gmail.com>
2020-09-21 20:40:59 +0300

    Updates

--- slider.d16eec5e.js
+++ slider.d16eec5e.js
@@ -13916,7 +13916,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49395" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52741" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
